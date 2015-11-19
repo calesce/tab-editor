e736c9ed3099bce71ae52f64abe0b7dd51e7d601
Cale Newman <newman.cale@gmail.com>
2015-11-19 00:21:24 -0600

    add support for separate measures

--- src/components/TabStaff.js
+++ src/components/TabStaff.js
@@ -1,58 +1,72 @@
 import React, { Component } from 'react';
 import TabNote from './TabNote';
 
+/*.map((note, i) => {
+  let color = (i === this.props.currentNoteIndex - 1) && this.props.isPlaying ? '#f9423a' : '#000000';
+
+  return {
+    string: note.string,
+    fret: note.fret,
+    duration: note.duration,
+    color: color
+  };
+});*/
+
 export default class TabStaff extends Component {
-  renderBars = () => {
+  renderBars = (i, measureLength) => {
     return (
       <g>
-        <rect x="0" y="10" width="100%" height="1.5" rx="0" ry="0" fill="#999999" stroke="#999999" strokeWidth="0.3" font="10pt Arial"></rect>
-        <rect x="0" y="23" width="100%" height="1.5" rx="0" ry="0" fill="#999999" stroke="#999999" strokeWidth="0.3" font="10pt Arial"></rect>
-        <rect x="0" y="36" width="100%" height="1.5" rx="0" ry="0" fill="#999999" stroke="#999999" strokeWidth="0.3" font="10pt Arial"></rect>
-        <rect x="0" y="49" width="100%" height="1.5" rx="0" ry="0" fill="#999999" stroke="#999999" strokeWidth="0.3" font="10pt Arial"></rect>
-        <rect x="0" y="62" width="100%" height="1.5" rx="0" ry="0" fill="#999999" stroke="#999999" strokeWidth="0.3" font="10pt Arial"></rect>
-        <rect x="0" y="75" width="100%" height="1.5" rx="0" ry="0" fill="#999999" stroke="#999999" strokeWidth="0.3" font="10pt Arial"></rect>
+        <rect x={250 * i} y='10' width={measureLength} height='0.5' rx='0' ry='0' fill='#000000' stroke='#000000' strokeWidth={0.1} font='10pt Arial'></rect>
+        <rect x={250 * i} y='23' width={measureLength} height='0.5' rx='0' ry='0' fill='#000000' stroke='#000000' strokeWidth='0.1' font='10pt Arial'></rect>
+        <rect x={250 * i} y='36' width={measureLength} height='0.5' rx='0' ry='0' fill='#000000' stroke='#000000' strokeWidth='0.1' font='10pt Arial'></rect>
+        <rect x={250 * i} y='49' width={measureLength} height='0.5' rx='0' ry='0' fill='#000000' stroke='#000000' strokeWidth='0.1' font='10pt Arial'></rect>
+        <rect x={250 * i} y='62' width={measureLength} height='0.5' rx='0' ry='0' fill='#000000' stroke='#000000' strokeWidth='0.1' font='10pt Arial'></rect>
+        <rect x={250 * i} y='75' width={measureLength} height='0.5' rx='0' ry='0' fill='#000000' stroke='#000000' strokeWidth='0.1' font='10pt Arial'></rect>
 
-        <rect x="0" y="10" width="1" height="65" rx="0" ry="0" fill="#999999" stroke="#999999" strokeWidth="0.3" font="10pt Arial"></rect>
-        <rect x="99.9%" y="10" width="1.5" height="65" rx="0" ry="0" fill="#999999" stroke="#999999" strokeWidth="0.3" font="10pt Arial"></rect>
+        <rect x={250 * i} y='10' width='0.5' height='65' rx='0' ry='0' fill='#000000' stroke='#000000' strokeWidth='0.1' font='10pt Arial'></rect>
+        <rect x={250 * i + measureLength } y='10' width='0.5' height='65' rx='0' ry='0' fill='#000000' stroke='#000000' strokeWidth='0.1' font='10pt Arial'></rect>
       </g>
     );
   }
 
-  renderNotes = (note, i) => {
+  renderMeasure = (i, measureLength, measure) => {
+    return (
+      <g>
+        { this.renderBars(i, measureLength) }
+        {
+          measure.notes.map((note, j) => this.renderNotes(note, i, j))
+        }
+      </g>
+    );
+  }
+
+  renderNotes = (note, measureNumber, index) => {
     return note.string.map((bleh, j) => {
+      let x = (measureNumber * 255) + (index * 55 + 40);
       let y = 80 - (13 * note.string[j]);
 
-      return <TabNote key={j} x={i * 100 + 100} y={y} color={note.color} fret={note.fret[j]} />
+      return <TabNote key={j} x={x} y={y} color={note.color} fret={note.fret[j]} />;
     });
   }
 
   render() {
+    let measureLength = 250;
+
     let style = {
       height: 250,
-      width: '80%'
+      width: measureLength
     };
 
-    let notes = this.props.notes.map((note, i) => {
-      let color = (i === this.props.currentNoteIndex - 1) && this.props.isPlaying ? '#f9423a' : '#000000';
+    let song = this.props.song;
 
-      return {
-        string: note.string,
-        fret: note.fret,
-        duration: note.duration,
-        color: color
-      };
-    });
-
-    return (<svg style={style}>
-
-      { this.renderBars() }
+    return (<svg style={{ width: '100%' }}>
       {
-        notes.map((note, i) => {
+        this.props.song.map((measure, i) => {
           return (
-            <g key={i}>
-              { this.renderNotes(note, i) }
+            <g key={i} style={style}>
+              { this.renderMeasure(i, measureLength, measure) }
             </g>
-          )
+          );
         })
       }
     </svg>);
