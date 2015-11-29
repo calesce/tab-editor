d5847ba0206b292f94cdb936ba01bc8f08ebdcbb
Cale Newman <newman.cale@gmail.com>
2015-11-28 18:08:49 -0600

    fit measures into row instead of static measure count

--- src/components/song.js
+++ src/components/song.js
@@ -49,61 +49,17 @@ let song = [
     notes: [
       {
         string: [1],
-        fret: [2],
+        fret: [7],
         duration: ['q'],
       },
       {
         string: [2],
-        fret: [0],
+        fret: [4],
         duration: ['q'],
       },
       {
         string: [3],
-        fret: [2],
-        duration: ['q'],
-      },
-      {
-        string: [4],
         fret: [0],
-        duration: ['q']
-      }
-    ]
-  },
-  {
-    timeSignature: '4/4',
-    notes: [
-      {
-        string: [1, 2],
-        fret: [3, 2],
-        duration: ['q', 'q']
-      },
-      {
-        string: [3],
-        fret: [0],
-        duration: ['q'],
-      },
-      {
-        string: [4],
-        fret: [3],
-        duration: ['h'],
-      }
-    ]
-  },{
-    timeSignature: '4/4',
-    notes: [
-      {
-        string: [1],
-        fret: [2],
-        duration: ['q'],
-      },
-      {
-        string: [2],
-        fret: [0],
-        duration: ['q'],
-      },
-      {
-        string: [3],
-        fret: [2],
         duration: ['q'],
       },
       {
@@ -118,43 +74,18 @@ let song = [
     notes: [
       {
         string: [1],
-        fret: [2],
+        fret: [0],
         duration: ['q'],
       },
       {
         string: [2],
-        fret: [0],
-        duration: ['q'],
-      },
-      {
-        string: [3],
         fret: [2],
         duration: ['q'],
       },
       {
-        string: [4],
-        fret: [0],
-        duration: ['q']
-      }
-    ]
-  },
-  {
-    timeSignature: '4/4',
-    notes: [
-      {
-        string: [1, 2],
-        fret: [3, 2],
-        duration: ['q', 'q']
-      },
-      {
-        string: [3],
-        fret: [0],
-        duration: ['q'],
-      },
-      {
-        string: [4],
-        fret: [3],
-        duration: ['h'],
+        string: [3, 4],
+        fret: [0, 1],
+        duration: ['h', 'h']
       }
     ]
   },
@@ -208,23 +139,43 @@ let song = [
     notes: [
       {
         string: [1],
-        fret: [2],
+        fret: [7],
         duration: ['q'],
       },
       {
         string: [2],
-        fret: [0],
+        fret: [4],
         duration: ['q'],
       },
       {
         string: [3],
-        fret: [2],
+        fret: [0],
         duration: ['q'],
       },
       {
         string: [4],
         fret: [0],
         duration: ['q']
+      }
+    ]
+  },
+  {
+    timeSignature: '4/4',
+    notes: [
+      {
+        string: [1],
+        fret: [0],
+        duration: ['q'],
+      },
+      {
+        string: [2],
+        fret: [2],
+        duration: ['q'],
+      },
+      {
+        string: [3, 4],
+        fret: [0, 1],
+        duration: ['h', 'h']
       }
     ]
   },
@@ -278,6 +229,51 @@ let song = [
     notes: [
       {
         string: [1],
+        fret: [7],
+        duration: ['q'],
+      },
+      {
+        string: [2],
+        fret: [4],
+        duration: ['q'],
+      },
+      {
+        string: [3],
+        fret: [0],
+        duration: ['q'],
+      },
+      {
+        string: [4],
+        fret: [0],
+        duration: ['q']
+      }
+    ]
+  },
+  {
+    timeSignature: '4/4',
+    notes: [
+      {
+        string: [1],
+        fret: [0],
+        duration: ['q'],
+      },
+      {
+        string: [2],
+        fret: [2],
+        duration: ['q'],
+      },
+      {
+        string: [3, 4],
+        fret: [0, 1],
+        duration: ['h', 'h']
+      }
+    ]
+  },
+  {
+    timeSignature: '4/4',
+    notes: [
+      {
+        string: [1],
         fret: [2],
         duration: ['q'],
       },
@@ -323,23 +319,43 @@ let song = [
     notes: [
       {
         string: [1],
-        fret: [2],
+        fret: [7],
         duration: ['q'],
       },
       {
         string: [2],
-        fret: [0],
+        fret: [4],
         duration: ['q'],
       },
       {
         string: [3],
-        fret: [2],
+        fret: [0],
         duration: ['q'],
       },
       {
         string: [4],
         fret: [0],
         duration: ['q']
+      }
+    ]
+  },
+  {
+    timeSignature: '4/4',
+    notes: [
+      {
+        string: [1],
+        fret: [0],
+        duration: ['q'],
+      },
+      {
+        string: [2],
+        fret: [2],
+        duration: ['q'],
+      },
+      {
+        string: [3, 4],
+        fret: [0, 1],
+        duration: ['h', 'h']
       }
     ]
   }
