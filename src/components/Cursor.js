fcadff47e5f81c2b5c14f01018f55bd1996199bf
Cale Newman <newman.cale@gmail.com>
2015-12-05 15:44:59 -0600

    more space when note has two digits

--- src/components/Cursor.js
+++ src/components/Cursor.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 
 export default class TabNote extends Component {
   render() {
-    let { x, y } = this.props;
+    let { x, y, fret } = this.props;
 
+    let width = fret > 9 ? 20 : 15;
     return (
       <g>
-        <rect x={x} y={y-12} height={0.1} width={14} strokeWidth={0.1} stroke='black'></rect>
-        <rect x={x} y={y+2} height={0.1} width={14} strokeWidth={0.1} stroke='black'></rect>
+        <rect x={x} y={y-12} height={0.1} width={width} strokeWidth={0.1} stroke='black'></rect>
+        <rect x={x} y={y+2} height={0.1} width={width} strokeWidth={0.1} stroke='black'></rect>
         <rect x={x} y={y-12} height={14} width={0.1} strokeWidth={0.1} stroke='black'></rect>
-        <rect x={x+14} y={y-12} height={14} width={0.1} strokeWidth={0.1} stroke='black'></rect>
+        <rect x={x + width} y={y-12} height={14} width={0.1} strokeWidth={0.1} stroke='black'></rect>
       </g>
     );
   }
