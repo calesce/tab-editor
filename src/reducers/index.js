96e21e4af9a23d5be50ff73c0f3b7475bdba3b6f
Cale Newman <newman.cale@gmail.com>
2015-12-22 23:11:40 -0600

    push up demo

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import tracks from './tracks';
-
-export default function rootReducer(state = {}, action) {
-  return {
-    tracks: tracks(state, action),
-    currentTrackIndex: state.currentTrackIndex
-  };
-}
