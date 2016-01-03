7600459e77ac3e97987662b896a45df310b6dbc8
Cale Newman <newman.cale@gmail.com>
2016-01-03 12:42:36 -0600

    use import-export monitor

--- src/containers/DevTools.js
+++ src/containers/DevTools.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import { createDevTools } from 'redux-devtools';
-import SliderMonitor from 'redux-slider-monitor';
-import DockMonitor from 'redux-devtools-dock-monitor';
+import { ImportExportMonitor } from 'redux-import-export-monitor';
+// import SliderMonitor from 'redux-slider-monitor';
+// import DockMonitor from 'redux-devtools-dock-monitor';
 
-export default createDevTools(
-  <DockMonitor toggleVisibilityKey='ctrl-h'
-             changePositionKey='ctrl-q'
-             defaultPosition='bottom'
-             defaultSize={0.15} defaultIsVisible={false}>
-    <SliderMonitor keyboardEnabled />
-  </DockMonitor>
-);
+export default createDevTools(<ImportExportMonitor />);
