892e37bcd3c9ccd03479329dcd201d1b4562b782
Cale Newman <newman.cale@gmail.com>
2016-01-09 18:26:21 -0600

    adjust size of font for tab clef based on how many strings there are

--- src/components/measure/Clef.js
+++ src/components/measure/Clef.js
@@ -15,12 +15,13 @@ export default class Clef extends Component {
     const { strings, y } = this.props;
     const baseY = strings * (3 + 1 / 3) + y; // start closer to top for lower number of strings
     const interval = strings * 4 - 4; // more spaced out for more strings
+    const fontSize = strings * 2 + 6; // the more strings, the larger the font default 18
 
     return (
       <g>
-        <text x={10} y={baseY} style={style} fill='#000000'>T</text>
-        <text x={10} y={baseY + interval} style={style} fill='#000000'>A</text>
-        <text x={10} y={baseY + (2 * interval)} style={style} fill='#000000'>B</text>
+        <text x={10} y={baseY} style={style} fill='#000000' fontSize={fontSize}>T</text>
+        <text x={10} y={baseY + interval} style={style} fill='#000000' fontSize={fontSize}>A</text>
+        <text x={10} y={baseY + (2 * interval)} style={style} fill='#000000' fontSize={fontSize}>B</text>
       </g>
     );
   }
