9c6a4f84d540e458b12bf27170958ca9bc67c3b3
Cale Newman <newman.cale@gmail.com>
2016-03-14 22:38:28 -0500

    added basic half note for music notation

--- src/components/measure/MusicNote.js
+++ src/components/measure/MusicNote.js
@@ -4,9 +4,32 @@ import shouldPureComponentUpdate from 'react-pure-render/function';
 export default class MusicNote extends Component {
   shouldComponentUpdate = shouldPureComponentUpdate;
 
-  renderQuarterStem = (x, color, stringOffset) => {
-    const y = 97 - (13 * stringOffset);
-    return <rect x={x + 5} y={y} height={25} width={1} fill={color}></rect>;
+  renderQuarterNote = (x, y, color) => {
+    return (
+      <svg x={x} y={y-7} style={{ overflow: 'visible' }}>
+        <g transform={'scale(0.75)'}>
+          <path strokeWidth={0.2} fill={color}
+            d='M 11.09297,35.38984 C 14.48881,33.56987 16.29825,30.27529 15.18519,27.79688 C 13.99793,25.15324 9.91818,24.40716 6.07861,26.13151 C 2.23905,27.85587 0.08645,31.40091 1.27371,34.04454 C 2.46098,36.68818 6.54072,37.43426 10.38029,35.70991 C 10.62026,35.60214 10.86657,35.51117 11.09297,34.38984 z '
+          />
+          <path strokeWidth={1.5} d='M 14.72547,29.05645 L 14.72547,0.46888' stroke={color} />
+        </g>
+      </svg>
+    );
+  };
+
+  renderHalfNote = (x, y, color) => {
+    return (
+      <svg x={x} y={y-7} style={{ overflow: 'visible' }}>
+        <g transform='matrix(-1,0,0,-1,254,262)'>
+          <path d='M 237.68484,218.18353 C 234.289,220.0035 232.47956,223.29808 233.59262,225.77649 C 234.77988,228.42013 238.85963,229.16621 242.6992,227.44186 C 246.53876,225.7175 248.69136,222.17246 247.5041,219.52883 C 246.31683,216.88519 242.23709,216.13911 238.39752,217.86346 C 238.15755,217.97123 237.91124,218.0622 237.68484,218.18353 z M 238.79457,220.42569 C 239.0358,220.30136 239.28005,220.20766 239.53576,220.09282 C 242.80883,218.62288 245.96997,218.55375 246.59187,219.93851 C 247.21377,221.32327 245.06209,223.64013 241.78902,225.11008 C 238.51594,226.58002 235.3548,226.64915 234.73291,225.26439 C 234.15959,223.98781 235.94804,221.89278 238.79457,220.42569 z '
+            style={{ opacity: 0.9, fill: color, fillOpacity:1, fillRule: 'evenOdd', strokeWidth: 0.2, strokeMiterlimit: 4, strokeDasharray: 'none', strokeDashoffset: 0, strokeOpacity: 1 }}
+          />
+          <path d='M 234.05234,224.51692 L 234.05234,258.10449'
+            style={{ stroke: color, strokeWidth: 1.5, strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeMiterlimit: 4, strokeDasharray: 'none', strokeOpacity: 1 }}
+          />
+        </g>
+      </svg>
+    );
   };
 
   renderEighthStem = (x, color, stringOffset) => {
@@ -30,12 +53,15 @@ export default class MusicNote extends Component {
     );
   };
 
-  renderStem = () => {
-    const { x, color, stringOffset } = this.props;
+  renderNote = (x, y, color) => {
 
     switch(this.props.note.duration) {
       case 'q':
-        return this.renderQuarterStem(x + 1, color, stringOffset);
+        return this.renderQuarterNote(x + 1, y, color);
+      case 'h':
+        return this.renderHalfNote(x + 1, y, color);
+      case 'w':
+        return this.renderWholeNote(x + 1, y, color);
       case 'e':
         return this.renderEighthStem(x + 1, color, stringOffset);
       case 's':
@@ -124,18 +150,9 @@ export default class MusicNote extends Component {
     }
 
     //  use rotate(180 9 31) to flip quarter note
-    const anote = fret !== undefined ?
-      <svg x={x} y={y-7} style={{ overflow: 'visible' }}>
-        <g transform={'scale(0.8)'}>
-          <path strokeWidth={0.2} fill={color}
-            d='M 11.09297,35.38984 C 14.48881,33.56987 16.29825,30.27529 15.18519,27.79688 C 13.99793,25.15324 9.91818,24.40716 6.07861,26.13151 C 2.23905,27.85587 0.08645,31.40091 1.27371,34.04454 C 2.46098,36.68818 6.54072,37.43426 10.38029,35.70991 C 10.62026,35.60214 10.86657,35.51117 11.09297,34.38984 z '
-          />
-          <path strokeWidth={1.5} d='M 14.72547,29.05645 L 14.72547,0.46888' stroke={color} />
-        </g></svg> : null;
-
     return (
       <g>
-        {anote}
+        {fret !== undefined ? this.renderNote(x, y, color) : null }
         {fret !== undefined ? this.renderDot(dotted, x, stringOffset, color) : null}
         {fret !== undefined ? this.renderTremolo(tremolo, x, stringOffset, color): null}
         {fret !== undefined ? this.renderVibrato(vibrato, x, color): null}
