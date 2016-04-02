2c01c121e18494ec53f70b5ef3fe5fd100272d57
Cale Newman <newman.cale@gmail.com>
2016-04-02 15:32:52 -0500

    remove unnecessary method and declaration

--- src/components/TabStaff.js
+++ src/components/TabStaff.js
@@ -21,29 +21,23 @@ class TabStaff extends Component {
     return 1.7 * (measures[measures.length - 1].rowIndex + 1) * (27 * tuning.length) + 40;
   };
 
-  renderMeasureForRow = (_, measureIndex) => {
-    return <Measure key={measureIndex} measureIndex={measureIndex} />;
-  };
-
   render() {
     const { layout, tuning, measures } = this.props;
 
-    let height = layout === 'linear' ? '100% - 10' : this.calcHeight(measures, tuning);
-    let width = layout === 'linear' ? this.calcWidth(measures) : window.innerWidth - 10;
+    const height = layout === 'linear' ? '100% - 10' : this.calcHeight(measures, tuning);
+    const width = layout === 'linear' ? this.calcWidth(measures) : window.innerWidth - 10;
     return (
       <div style={{ ...style, width, height }}>
-        { this.props.measures.map(this.renderMeasureForRow) }
+      { this.props.measures.map((_, i) => <Measure key={i} measureIndex={i} />) }
       </div>
     );
   }
 }
 
 function mapStateToProps(state) {
-  const track = state.tracks[state.currentTrackIndex];
-
   return {
-    measures: track.measures,
-    tuning: track.tuning,
+    measures: state.tracks[state.currentTrackIndex].measures,
+    tuning: state.tracks[state.currentTrackIndex].tuning,
     layout: state.layout
   };
 }
