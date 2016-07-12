1a756a697f0169386f39679c65ac92c5ffa3a9fe
Cale Newman <newman.cale@gmail.com>
2016-07-11 20:42:27 -0500

    enable Chrome devtools extension

--- src/util/configureStore.dev.js
+++ src/util/configureStore.dev.js
@@ -2,7 +2,9 @@ import { createStore } from 'redux';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState);
+  const store = createStore(rootReducer, initialState,
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  );
 
   if(module.hot) {
     module.hot.accept('../reducers', () => {
