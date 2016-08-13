a88bb6f397c930abd1377b7f96fd70402e1f9212
Cale Newman <newman.cale@gmail.com>
2016-08-13 15:23:47 -0500

    let/const available in Node 6

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-/*eslint-disable no-var,no-console */
-var path = require('path');
-var express = require('express');
-var webpack = require('webpack');
-var config = require('./webpack.config.dev');
+/*eslint-disable no-console */
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const config = require('./webpack.config.dev');
 
-var app = express();
-var compiler = webpack(config);
+const app = express();
+const compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
@@ -19,9 +19,8 @@ app.get('*', function(req, res) {
 });
 
 app.listen(3000, 'localhost', function(err) {
-  if (err) {
-    console.log(err);
-    return;
+  if(err) {
+    return console.log(err);
   }
 
   console.log('Listening at http://localhost:3000');
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,5 @@
-/*eslint-disable no-var */
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
