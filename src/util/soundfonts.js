8b496b5c5c580a6a16b91fe8021151e69c87295b
Cale Newman <newman.cale@gmail.com>
2016-10-05 23:03:35 -0500

    fix soundfonts

--- src/util/soundfonts.js
+++ src/util/soundfonts.js
@@ -3,9 +3,10 @@ import Soundfont from 'soundfont-player';
 import audioContext from '../util/audioContext';
 
 // Fix for Safari/Edge, which can't play .ogg files
+let options = {};
 if(!!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/) || !!navigator.userAgent.match(/Edge\/\d+/)) {
-  Soundfont.nameToUrl = function(name: string): string {
-    return `https://cdn.rawgit.com/gleitz/midi-js-Soundfonts/master/FluidR3_GM/${name}-mp3.js`;
+  options.nameToUrl = function(name: string): string {
+    return `https://raw.githubusercontent.com/gleitz/midi-js-soundfonts/gh-pages/FluidR3_GM/${name}-mp3.js`;
   };
 }
 
@@ -17,9 +18,9 @@ export function loadSoundfonts(instruments: Array<string>): Promise<Object> {
   });
 
   return Promise.all(promises)
-    .then(instrumentBuffers => {
-      instrumentBuffers.forEach((buffer, i) => {
-        bufferCache[instruments[i]] = buffer;
+    .then(instrumentObjects => {
+      instrumentObjects.forEach((instrument, i) => {
+        bufferCache[instruments[i]] = instrument.buffers;
       });
       return bufferCache;
     });
@@ -29,5 +30,5 @@ const loadSoundfont = (instrument: string, cache: Object): Promise<Object> => {
   if(cache[instrument]) {
     return new Promise(resolve => resolve(cache[instrument]));
   }
-  return Soundfont.loadBuffers(audioContext, instrument);
+  return Soundfont.instrument(audioContext, instrument, options);
 };
