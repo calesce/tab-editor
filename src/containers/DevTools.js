934299715cb72955460c39970c1e64d3a584bf03
Cale Newman <newman.cale@gmail.com>
2016-10-22 13:54:36 -0500

    remove unnecessary files

--- src/containers/DevTools.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createDevTools } from 'redux-devtools';
-import { ImportExportMonitor } from 'redux-import-export-monitor';
-// import SliderMonitor from 'redux-slider-monitor';
-// import DockMonitor from 'redux-devtools-dock-monitor';
-
-export default createDevTools(<ImportExportMonitor />);
--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,5 +1,14 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./Root.prod');
-} else {
-  module.exports = require('./Root.dev');
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import App from './App';
+
+export default class Root extends Component {
+  render() {
+    const { store } = this.props;
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  }
 }
