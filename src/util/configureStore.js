7632e62073677d5bcef40b4af15bd0d5e9669b06
Cale Newman <newman.cale@gmail.com>
2017-01-29 16:18:00 -0600

    update some deps and remove CommonJS from client-side code

--- src/util/configureStore.js
+++ src/util/configureStore.js
@@ -1,5 +1,19 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.prod');
-} else {
-  module.exports = require('./configureStore.dev');
+/* @flow */
+
+import { createStore } from 'redux';
+import rootReducer from '../reducers';
+
+import type { State } from './stateTypes';
+
+export default function configureStore(initialState: State) {
+  const store = createStore(rootReducer, initialState);
+
+  if (module.hot) {
+    module.hot.accept('../reducers', () => {
+      const nextReducer = require('../reducers').default;
+      store.replaceReducer(nextReducer);
+    });
+  }
+
+  return store;
 }
--- src/util/configureStore.prod.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-import { createStore } from 'redux';
-import rootReducer from '../reducers';
-
-import type { State } from './stateTypes';
-
-export default function configureStore(initialState: State) {
-  return createStore(rootReducer, initialState);
-}
